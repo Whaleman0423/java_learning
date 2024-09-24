e920f316e2d81a3c3e82b45be7a6b3f892b00491
Whaleman0423 <sheiyuray@gmail.com>
2024-09-24 18:39:52 +0800

    array 練習, 初始化、結合迴圈與Scanner user input

---
+++ 08/Array.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // // 陣列的基本操作練習
+        // int[] grades = new int[3];
+        // System.out.println("陣列長度：" + grades.length);
+
+        // // 查看 3 個元素的值，預設為 0
+        // System.out.println(grades[0]);
+        // System.out.println(grades[1]);
+        // System.out.println(grades[2]);
+        // // System.out.println(grades[3]); // 錯誤：超過陣列的長度
+
+        // grades[0] = 70;
+        // grades[1] = 80;
+        // grades[2] = 90;
+        // // System.out.println(grades[0]);
+        // // System.out.println(grades[1]);
+        // // System.out.println(grades[2]);
+
+        // for (int i = 0; i < grades.length; i++) {
+        // System.out.println(grades[i]);
+        // }
+
+        // int[] grades = new int[] { 70, 80, 90, 100 };
+        // System.out.println("陣列長度：" + grades.length);
+        // for (int i = 0; i < grades.length; i++) {
+        // System.out.println(grades[i]);
+        // }
+
+        // // 輸入學生的數量，以及每位學生的成績，計算平均數
+        // System.out.println("請輸入有幾位學生？");
+        // Scanner s = new Scanner(System.in);
+
+        // int number = s.nextInt();
+
+        // int[] grades = new int[number];
+
+        // System.out.println("請逐一輸入學生的成績：");
+        // for (int i = 0; i < grades.length; i++) {
+        // grades[i] = s.nextInt();
+        // }
+
+        // int total = 0;
+        // for (int i = 0; i < grades.length; i++) {
+        // System.out.println(grades[i]);
+        // total += grades[i];
+        // }
+
+        // // 擇一轉成 double 即可
+        // double average = (double) total / grades.length;
+        // // double average = total /(double) grades.length;
+
+        // System.out.println("學生的平均分數為：" + average);
+    }
+}
