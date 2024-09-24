599ca4f1b8a56a5fec84b1effa76e02469bcbed2
Whaleman0423 <sheiyuray@gmail.com>
2024-09-24 15:30:31 +0800

    迴圈基礎

---
+++ 06/Loop.java
@@ -0,0 +1,61 @@
+
+public class Loop {
+    public static void main(String[] args) {
+        // while 迴圈
+        // // 無窮迴圈
+        // while (true) {
+        // System.out.println("Loop");
+        // }
+
+        // // 可以被控制運作次數的迴圈
+        // int x = 2;
+        // while (x <= 6) {
+        // System.out.println(x);
+        // x++;
+        // }
+
+        // 計算 1+2+3+4+5 的總和
+        // int total = 0;
+
+        // 純加法
+        // total += 1;
+        // total += 2;
+        // total += 3;
+        // total += 4;
+        // total += 5;
+        // System.out.println(total);
+
+        // // while 迴圈寫法
+        // int n = 1;
+        // int max = 100;
+        // while (n <= max) {
+        // System.out.println(n);
+        // total += n;
+        // n++;
+        // }
+        // System.out.println(total);
+
+        // // for 迴圈
+        // // for (int x = 2; x <= 6; x = x + 2) {
+        // for (int x = 2; x <= 6; x++) {
+        // System.out.println(x);
+        // total += x;
+        // }
+        // System.out.println();
+
+        // // 計算 1+2+3+4+5 的總和
+        // int total = 0;
+        // for (int n = 1; n <= 5; n++) {
+        // total = total + n;
+        // }
+        // System.out.println(total); // 15
+
+        // // 計算 1+2+3+...+100 的總和
+        // int total = 0;
+        // for (int n = 1; n <= 100; n++) {
+        // total = total + n;
+        // }
+        // System.out.println(total); // 5050
+
+    }
+}
