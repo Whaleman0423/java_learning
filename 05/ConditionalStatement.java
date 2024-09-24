94f96733aaac7410200312481039df6ebd5a41b6
Whaleman0423 <sheiyuray@gmail.com>
2024-09-24 14:46:36 +0800

    標準輸入、if-else、switch 流程設計

---
+++ 05/ConditionalStatement.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        // // 使用標準輸入（從終端機輸入資料）
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("請輸入一個整數：");
+        // int x = s.nextInt();
+        // System.out.println(x * 10);
+
+        // // if 判斷式
+        // if (true) {
+        // System.out.println("執行");
+        // }
+        // if (false) {
+        // System.out.println("執行");
+        // }
+
+        // if (false) {
+        // System.out.println("正面");
+        // } else {
+        // System.out.println("反面");
+        // }
+
+        // if (true) {
+        // System.out.println("正面");
+        // } else {
+        // System.out.println("反面");
+        // }
+
+        // // 結合標準輸入與 if 判斷式
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("請問你想領多少錢？");
+        // int money = s.nextInt();
+
+        // if (money > 100000) {
+        // System.out.println("太多了，不能領");
+        // } else if (money >= 100) {
+        // System.out.println("OK，💰在這裡");
+        // } else {
+        // System.out.println("錢太少了，不能領");
+        // }
+
+        // // switch 判斷式
+        // int n1 = 3;
+        // int n2 = 4;
+        // System.out.println("請輸入想要做的運算（支援 + - * /）");
+        // Scanner s = new Scanner(System.in);
+        // String op = s.next();
+
+        // System.out.println("使用者輸入的運算符號是");
+        // System.out.println(op);
+
+        // switch (op) {
+        // case "+":
+        // System.out.println(n1 + n2);
+        // break;
+        // case "-":
+        // System.out.println(n1 - n2);
+        // break;
+        // case "*":
+        // System.out.println(n1 * n2);
+        // break;
+        // case "/":
+        // System.out.println(n1 / n2);
+        // break;
+        // default:
+        // System.out.println("不支援的運算符號");
+        // break;
+        // }
+    }
+}
