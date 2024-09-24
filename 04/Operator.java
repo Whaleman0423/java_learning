b0fa395e63143659fdc895a7f92a3d1096ebe114
Whaleman0423 <sheiyuray@gmail.com>
2024-09-24 13:46:30 +0800

    完成java運算子學習

---
+++ 04/Operator.java
@@ -0,0 +1,90 @@
+public class Operator {
+  public static void main(String[] args) {
+    // // 算術運算
+    // int x = 3 + 4;
+    // System.out.println(x);
+
+    // x = 3 / 4; // x 還是 int，只取整數
+    // System.out.println(x);
+
+    // x = 5 + (3 / 4);
+    // System.out.println(x);
+
+    // // int y = 5 / 2.0; // 錯誤：2.0 是 double，是小數的除法，會得到 2.5，無法放入 int y
+    // double y = 5 / 2.0;
+    // System.out.println(y);
+
+    // double z = 5 % 3.5;
+    // System.out.println(z);
+
+    // 指定運算
+    // int x = 5; // x 即為指定運算（賦值）
+    // // x + 2; // 錯誤
+    // x = x + 2;
+    // System.out.println(x);
+
+    // int y = 3;
+    // y += 2;
+    // System.out.println(y);
+
+    // y *= 3;
+    // System.out.println(y);
+
+    // // 比較運算
+    // // int x = 3 > 5; // 錯誤，是會回傳布林值
+    // boolean x = 3 > 5;
+    // System.out.println(x); // false
+
+    // x = 3 < 5;
+    // System.out.println(x); // true
+
+    // boolean y = 5 <= 5;
+    // System.out.println(y); // true
+
+    // y = 2 != 2;
+    // System.out.println(y); // false
+
+    // boolean z = "Hello" == "Hello";
+    // System.out.println(z); // true
+
+    // z = "Hello" == "hello"; // h 大小寫不同
+    // System.out.println(z); // false
+
+    // // 單元運算
+    // int x = 4;
+    // x++;
+    // System.out.println(x);
+
+    // x--;
+    // System.out.println(x);
+
+    // boolean y = !true; // 布林反轉
+    // System.out.println(y); // false
+
+    // // 邏輯運算
+    // boolean x = true && true;
+    // System.out.println(x); // true
+
+    // x = true && false;
+    // System.out.println(x); // false
+
+    // x = false && true;
+    // System.out.println(x); // false
+
+    // x = false && false;
+    // System.out.println(x); // false
+
+    // boolean y = true || true;
+    // System.out.println(y); // true
+
+    // y = true || false;
+    // System.out.println(y); // true
+
+    // y = false || true;
+    // System.out.println(y); // true
+
+    // y = false || false;
+    // System.out.println(y); // false
+  }
+
+}
